ca40248519d9fc75cb2ef052510d1bf6c3d36bcb
malachi-constant <lucascchanson@gmail.com>
2022-02-17 04:15:11 +0000

    Deploying to gh-pages from @ malachi-constant/pages-test@fb2328050b55dfe9510c18ff95bb96899d1b4730 🚀

---
+++ bin/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+version="$1"
+echo "version: ${version}"
+
+mkdir releases/${version}
+
+cat >releases/${version}/index.html <<EOL
+<!doctype html>
+<html>
+  <head>
+    <title>This is the title of the webpage!</title>
+  </head>
+  <body>
+    <p>Version: ${version}</p>
+  </body>
+</html> 
+EOL
+
